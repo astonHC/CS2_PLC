272c0a41a6c9cc7bcfbfd3c52b96bc2cfb7a2b43
astonHC <139020900+astonHC@users.noreply.github.com>
2024-09-07 00:08:52 +0100

    Add files via upload

---
+++ lab1/util.h
@@ -0,0 +1,31 @@
+/* COPYRIGHT (C) HARRY CLARK 2024 */
+
+/* CS2_PLC DEMONSTRATIONS */
+
+/* THIS FILE PERTAINS TOWARDS THE FUNCTIONALITY OF DISCERNING THE TOTAL */
+/* WHEN TWO INTEGERS ARE ADDED */
+
+#ifndef CS2_PLC_LAB_1
+#define CS2_PLC_LAB_1
+
+/* SYSTEM INCLUDES */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#if defined(USE_UTIL)
+#define USE_UTIL
+#else
+#define USE_UTIL
+
+#define     ADD(VALUE_A, VALUE_B)               ((VALUE_A) + (VALUE_B))
+#define     MULTIPLY(VALUE_A, VALUE_B)          ((VALUE_A) * (VALUE_B))
+#define     SUBTRACT(VALUE_A, VALUE_B)          ((VALUE_A) - (VALUE_B))
+#define     DIVIDE(VALUE_A, VALUE_B)            ((VALUE_B) != 0 ? ((VALUE_A) / (VALUE_B)) : -1)
+
+int OPERATION_TYPE(int, int);
+
+#endif
+#endif
