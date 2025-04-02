ede7e6b368bdd000a99b80468551ea0459f69c82
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-04-02 17:08:03 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,7 +1,14 @@
+from my_print import func, getString
 from add import add
+
 
 def main():
     add()
+    s = "Harry"
+
+    ## ACCESS FUNC TO USE THE FPTR OF GETSTRING
+
+    print(func(getString, "good morning", "up"))
 
 if __name__ == "__main__":
     main()
