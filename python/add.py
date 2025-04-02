7fd1d41d94036da088b5cb73f3428430fdde7068
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-04-02 16:38:39 +0100

    added python practice stuff

---
+++ python/add.py
@@ -0,0 +1,11 @@
+def add():
+    x = 5
+    y = 10
+    z = x + y
+    print("The sum of x and y is:" + str(z))
+
+def main():
+    add()
+
+if __name__ == "__main__":
+    main()
---
+++ python/main.py
@@ -0,0 +1,7 @@
+from add import add
+
+def main():
+    add()
+
+if __name__ == "__main__":
+    main()
