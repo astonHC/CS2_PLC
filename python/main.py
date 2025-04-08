57ce55c6b85c6b1404fc9af4460e88fbc6365287
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-04-08 16:35:08 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -10,5 +10,9 @@ def main():
 
     print(func(getString, "good morning", "up"))
 
+    ## LAMBDA EXP.
+
+    print("the elements of the list are: ", list2)
+
 if __name__ == "__main__":
     main()
